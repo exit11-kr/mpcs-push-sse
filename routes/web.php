5c1cae27a8c406a06bca08031b9e93c42e18dbb4
hongchr <hongchr@naver.com>
2023-02-17 17:27:15 +0900

    0.3.0 multi-tenancy

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Routing\Router;
 use Mpcs\Core\Facades\Core;
+use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
+use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
+
+$tenantyMiddlewares = [InitializeTenancyByDomain::class, PreventAccessFromCentralDomains::class];
 
 // Push SSE Route
 Route::group([
     'as'            => "stream.",
     'prefix'        => "stream",
     'namespace'     => 'Mpcs\PushSse\Http\Controllers',
-    'middleware'    => ['web'],
+    'middleware'    => array_merge($tenantyMiddlewares, ['web']),
 ], function (Router $router) {
     $router->get('push_sse', 'PushSseController@stream')->name('push_sse');
 });
