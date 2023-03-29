3b1d8c64f4d7a0b3803fcd83224a3c82d34459a2
hongchr <hongchr@naver.com>
2023-03-29 13:43:35 +0900

    universal migration

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Routing\Router;
 use Mpcs\Core\Facades\Core;
-use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
-use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
-
-$tenantyMiddlewares = [InitializeTenancyByDomain::class, PreventAccessFromCentralDomains::class];
 
 // Push SSE Route
 Route::group([
     'as'            => "stream.",
     'prefix'        => "stream",
     'namespace'     => 'Mpcs\PushSse\Http\Controllers',
-    'middleware'    => array_merge($tenantyMiddlewares, ['web']),
+    'middleware'    => Core::getUniversalMiddlewares('ui'),
 ], function (Router $router) {
     $router->get('push_sse', 'PushSseController@stream')->name('push_sse');
 });
